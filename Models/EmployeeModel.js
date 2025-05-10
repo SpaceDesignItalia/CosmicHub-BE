ec164c315e242f8fdb2353e8e568de7ff7552a2e
Marco Macherelli <elmache05@gmail.com>
2025-05-10 12:54:31 +0200

    Update Password

--- Models/EmployeeModel.js
+++ Models/EmployeeModel.js
@@ -1,62 +1,135 @@
 // Models/EmployeeModel.js
+const bcrypt = require("bcrypt");
+
 class EmployeeModel {
-  static async getAllEmployees(db) {
-    const query = `SELECT * FROM public."User"`;
-    const result = await db.query(query);
-    console.log(result.rows);
-    return result.rows;
+  static getAllEmployees(db) {
+    return new Promise((resolve, reject) => {
+      const query = `SELECT * FROM public."User"`;
+      db.query(query, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result.rows);
+        }
+      });
+    });
   }
 
-  static async getEmployeeById(db, employeeId) {
-    const query = `SELECT * FROM employee WHERE id = $1`;
-    const result = await db.query(query, [employeeId]);
-    return result.rows[0];
+  static getEmployeeById(db, employeeId) {
+    return new Promise((resolve, reject) => {
+      const query = `SELECT * FROM employee WHERE id = $1`;
+      db.query(query, [employeeId], (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result.rows[0]);
+        }
+      });
+    });
   }
 
-  static async searchEmployee(db, searchTerm) {
-    const likeTerm = `%${searchTerm}%`;
-    const query = `
-      SELECT * FROM employee
-      WHERE name LIKE $1 OR surname LIKE $2 OR email LIKE $3 OR phone LIKE $4
-    `;
-    const result = await db.query(query, [likeTerm, likeTerm, likeTerm, likeTerm]);
-    return result.rows;
+  static searchEmployee(db, searchTerm) {
+    return new Promise((resolve, reject) => {
+      const likeTerm = `%${searchTerm}%`;
+      const query = `
+        SELECT * FROM employee
+        WHERE name LIKE $1 OR surname LIKE $2 OR email LIKE $3 OR phone LIKE $4
+      `;
+      db.query(
+        query,
+        [likeTerm, likeTerm, likeTerm, likeTerm],
+        (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(result.rows);
+          }
+        }
+      );
+    });
   }
 
-  static async createNewEmployee(db, data) {
-    const query = `
-      INSERT INTO employee (name, surname, email, phone, role)
-      VALUES ($1, $2, $3, $4, $5) RETURNING id
-    `;
-    const result = await db.query(query, [
-      data.name,
-      data.surname,
-      data.email,
-      data.phone,
-      data.role,
-    ]);
-    return result.rows[0].id;
+  static createNewEmployee(db, data) {
+    return new Promise((resolve, reject) => {
+      const query = `
+        INSERT INTO employee (name, surname, email, phone, role)
+        VALUES ($1, $2, $3, $4, $5) RETURNING id
+      `;
+      db.query(
+        query,
+        [data.name, data.surname, data.email, data.phone, data.role],
+        (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(result.rows[0].id);
+          }
+        }
+      );
+    });
   }
 
-  static async updateEmployeeData(db, data) {
-    const query = `
-      UPDATE employee
-      SET name = $1, surname = $2, email = $3, phone = $4, role = $5
-      WHERE id = $6
-    `;
-    await db.query(query, [
-      data.name,
-      data.surname,
-      data.email,
-      data.phone,
-      data.role,
-      data.id,
-    ]);
+  static updateEmployeeData(db, data) {
+    return new Promise((resolve, reject) => {
+      const query = `
+        UPDATE employee
+        SET name = $1, surname = $2, email = $3, phone = $4, role = $5
+        WHERE id = $6
+      `;
+      db.query(
+        query,
+        [data.name, data.surname, data.email, data.phone, data.role, data.id],
+        (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(result);
+          }
+        }
+      );
+    });
   }
 
-  static async deleteEmployee(db, employeeId) {
-    const query = `DELETE FROM employee WHERE id = $1`;
-    await db.query(query, [employeeId]);
+  static deleteEmployee(db, employeeId) {
+    return new Promise((resolve, reject) => {
+      const query = `DELETE FROM employee WHERE id = $1`;
+      db.query(query, [employeeId], (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+
+  static async updateUserPassword(db, email, currentPassword, newPassword) {
+    return new Promise((resolve, reject) => {
+      const hashOldPassword = bcrypt.hashSync(currentPassword, 10);
+      const searchUserQuery = `SELECT * FROM public."User" WHERE "email" = $1 AND "password" = $2`;
+
+      db.query(searchUserQuery, [email, hashOldPassword], (error, result) => {
+        if (error) {
+          reject(error);
+        } else if (result.rows.length === 0) {
+          const hashNewPassword = bcrypt.hashSync(newPassword, 10);
+          const updateUserPasswordQuery = `UPDATE public."User" SET "password" = $1 WHERE "email" = $2 AND "password" = $3`;
+          db.query(
+            updateUserPasswordQuery,
+            [hashNewPassword, email, hashOldPassword],
+            (error, result) => {
+              if (error) {
+                reject(error);
+              } else {
+                resolve(true);
+              }
+            }
+          );
+        } else {
+          resolve(false);
+        }
+      });
+    });
   }
 }
 
--- Routes/Employee/employeeUPDATE.js
+++ Routes/Employee/employeeUPDATE.js
@@ -4,13 +4,15 @@ const EmployeeController = require("../../Controllers/EmployeeController");
 const authenticateMiddleware = require("../../middlewares/Authentication/Authmiddleware");
 
 const employeeUPDATE = (db) => {
-  router.put("/UpdateEmployeeData", authenticateMiddleware, (req, res) => { 
+  router.put("/UpdateEmployeeData", authenticateMiddleware, (req, res) => {
     EmployeeController.updateEmployeeData(req, res, db);
   });
 
-  
+  router.put("/UpdateUserPassword", authenticateMiddleware, (req, res) => {
+    EmployeeController.updateUserPassword(req, res, db);
+  });
 
   return router;
 };
 
-module.exports = employeeUPDATE; 
+module.exports = employeeUPDATE;
